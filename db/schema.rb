4f2c7550e4a617c1e75c9b057f7c6751e217558a
nforne <mnforne@gmail.com>
2022-01-08 05:04:18 -0500

    setup project

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.puts 'Establishing connection to database ...'
 
 ActiveRecord::Schema.define(version: 20160625062916) do
 
